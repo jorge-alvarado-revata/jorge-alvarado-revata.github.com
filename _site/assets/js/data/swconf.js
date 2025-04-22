16ca9e96cbdd28c7bf9ec578be3242c0cb8abf30
jorge-alvarado-revata <serviciosplusapp@gmail.com>
2025-04-22 21:41:08 +0000

    update site

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1745358034',resources: [
+      '/_site/assets/css/jekyll-theme-chirpy.css',
+      '/_site/',
+      
+        '/_site/categories/',
+      
+        '/_site/tags/',
+      
+        '/_site/archives/',
+      
+        '/_site/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
